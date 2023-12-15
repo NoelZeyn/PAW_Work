6f8a34b592e68effdb540298f3445739853dd25f
ArayaFeisa <arayafeisas@gmail.com>
2023-12-15 17:27:25 +0700

    css style for create and index

--- PAW/resources/views/payments/create.blade.php
+++ PAW/resources/views/payments/create.blade.php
@@ -1,18 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Create Payment</h1>
-<form method="post" action="{{ route('payments.store') }}">
-    @csrf
-    <label for="order_id">Order ID:</label>
-    <input type="text" name="order_id" required>
-    <br>
-    <label for="amount">Amount:</label>
-    <input type="text" name="amount" required>
-    <br>
-    <label for="payment_method">Payment Method:</label>
-    <input type="text" name="payment_method" required>
-    <br>
-    <button type="submit">Submit Payment</button>
-</form>
+    <div class="payments-create-container">
+        <h1>Create Payment</h1>
+        <form method="post" action="{{ route('payments.store') }}" class="payment-form">
+            @csrf
+            <div class="form-group">
+                <label for="order_id">Order ID:</label>
+                <input type="text" name="order_id" required>
+            </div>
+            <div class="form-group">
+                <label for="amount">Amount:</label>
+                <input type="text" name="amount" required>
+            </div>
+            <div class="form-group">
+                <label for="payment_method">Payment Method:</label>
+                <input type="text" name="payment_method" required>
+            </div>
+            <button type="submit">Submit Payment</button>
+        </form>
+    </div>
+
+    <style>
+
+        .payments-create-container {
+            text-align: center;
+        }
+
+        .payment-form {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            width: 300px;
+            margin: 0 auto;
+        }
+
+        .payment-form label {
+            display: block;
+            margin-bottom: 8px;
+            color: #555;
+        }
+
+        .payment-form input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 12px;
+            box-sizing: border-box;
+        }
+
+        .payment-form button {
+            background-color: #4caf50;
+            color: #fff;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        .payment-form button:hover {
+            background-color: #45a049;
+        }
+    </style>
 @endsection
--- PAW/resources/views/payments/index.blade.php
+++ PAW/resources/views/payments/index.blade.php
@@ -1,29 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Payment List</h1>
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Order ID</th>
-            <th>Amount</th>
-            <th>Payment Method</th>
-            <th>Status</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($payments as $payment)
-            <tr>
-                <td>{{ $payment->id }}</td>
-                <td>{{ $payment->order_id }}</td>
-                <td>{{ $payment->amount }}</td>
-                <td>{{ $payment->payment_method }}</td>
-                <td>{{ $payment->status }}</td>
-                <td><a href="{{ route('payments.show', $payment->id) }}">View Details</a></td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+    <div class="container">
+        <div class="payment-list">
+            <h1>Payment List</h1>
+            
+            <div class="payments-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Order ID</th>
+                            <th>Amount</th>
+                            <th>Payment Method</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($payments as $payment)
+                            <tr>
+                                <td>{{ $payment->id }}</td>
+                                <td>{{ $payment->order_id }}</td>
+                                <td>{{ $payment->amount }}</td>
+                                <td>{{ $payment->payment_method }}</td>
+                                <td>{{ $payment->status }}</td>
+                                <td><a href="{{ route('payments.show', $payment->id) }}" class="view-details">View Details</a></td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="6">No payments available</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            background-color: #f5f5f5;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 960px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .payment-list h1 {
+            margin-bottom: 20px;
+        }
+
+        .payments-table table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        .payments-table th, .payments-table td {
+            border: 1px solid #ddd;
+            padding: 12px;
+            text-align: left;
+        }
+
+        .payments-table th {
+            background-color: #f2f2f2;
+        }
+
+        .view-details {
+            color: #007bff;
+            text-decoration: none;
+        }
+
+        .view-details:hover {
+            text-decoration: underline;
+        }
+
+        @media (max-width: 768px) {
+            .container {
+                padding: 10px;
+            }
+        }
+    </style>
 @endsection
