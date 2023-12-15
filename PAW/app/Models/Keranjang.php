23b93d73083e299470f9f03813e34a116f6e3d8c
Aksamala Citrayuh Anasita <aksamala9999@gmail.com>
2023-12-15 15:58:35 +0700

    a

--- PAW/app/Models/Keranjang.php
+++ PAW/app/Models/Keranjang.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User; // Import model User jika belum diimport
 
 class Keranjang extends Model
 {
@@ -18,9 +17,8 @@ class Keranjang extends Model
         'amount',
         'user_id'
     ];
-
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id'); // Sesuaikan foreign key dengan 'user_id'
+        return $this->belongsTo(User::class);
     }
 }
