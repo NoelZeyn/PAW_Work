e73a889dbd33d34ab6d8f013859cb2ab972927c5
Aksamala Citrayuh Anasita <aksamala9999@gmail.com>
2023-12-15 16:09:50 +0700

    del

--- PAW/routes/web.php
+++ PAW/routes/web.php
@@ -9,8 +9,6 @@ use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PaymentsController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\KeranjangController;
-
 
 Route::post('/cart/add/', [InventoryController::class, 'add'])->name('inventory.add');
 
@@ -27,10 +25,3 @@ Route::resource('products', ProductController::class);
 Route::resource('reviews', ReviewController::class);
 Route::resource('payments', PaymentsController::class);
 Route::resource('users', UserController::class);
-
-//aksa
-Route::post('/keranjang/tambah', [KeranjangController::class, 'tambahKeKeranjang']);
-Route::put('/keranjang/{id}/perbarui', [KeranjangController::class, 'perbaruiItemKeranjang']);
-Route::delete('/keranjang/{id}/hapus', [KeranjangController::class, 'hapusItemKeranjang']);
-
-Route::resource('keranjang', KeranjangController::class);
