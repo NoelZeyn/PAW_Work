ad5b09c670e5f4616aa1bf1b89f7abf8e740db3a
Aksamala Citrayuh Anasita <aksamala9999@gmail.com>
2023-12-15 15:32:59 +0700

    hapus

--- PAW/routes/web.php
+++ PAW/routes/web.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PaymentsController;
-use App\Http\Controllers\KeranjangController;
 use App\Http\Controllers\DashboardController;
 
 
@@ -28,9 +27,3 @@ Route::resource('reviews', ReviewController::class);
 Route::resource('payments', PaymentsController::class);
 Route::resource('users', UserController::class);
 
-//aksa
-Route::get('/keranjang/add', [KeranjangController::class, 'tambahKeKeranjang'])->name('keranjang.add');
-Route::put('/keranjang/update/{id}', [KeranjangController::class, 'perbaruiItemKeranjang'])->name('keranjang.update');
-Route::delete('/keranjang/delete/{id}', [KeranjangController::class, 'hapusItemKeranjang'])->name('keranjang.delete');
-
-Route::resource('keranjang', KeranjangController::class);
