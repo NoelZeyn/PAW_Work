e8021597e00d763a56cc4a5f187fd9ca41cbfc76
NoelZeyn <ahmadakrom50@gmail.com>
2023-12-15 20:47:34 +0700

    Update dashboard.blade.php

--- PAW/resources/views/layouts/dashboard.blade.php
+++ PAW/resources/views/layouts/dashboard.blade.php
@@ -7,35 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your App Title</title>
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
-<<<<<<< Updated upstream
-    <!-- Add this to the head section of your HTML file -->
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-    <style>
-
-        nav {
-            position: relative;
-            background-color: #032539; /* Warna 032539 */
-
-        }
-
-        .keranjang-icon {
-            position: absolute;
-            top: 10px; 
-            right: 10px;
-        }
-        .keranjang-icon a i {
-            color: #FA991C; /* Warna FA991C */
-        }
-
-
-    </style>
-=======
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
->>>>>>> Stashed changes
 
 <script>
     $(document).ready(function () {
+        // Add a smooth scroll effect for category links
         $('.category-scroll a').on('click', function (event) {
             if (this.hash !== '') {
                 event.preventDefault();
@@ -64,11 +40,6 @@
             <li><a href="{{ route('products.index') }}">Products</a></li>
             <li><a href="{{ route('reviews.index') }}">Reviews</a></li>
             <li><a href="{{ route('users.index') }}">Users</a></li>
-            <li class="keranjang-icon">
-            <a href="{{ route('keranjang.index') }}">
-                <i class="fa fa-shopping-cart"></i>
-            </a>
-        </li>
             <!-- Add more navigation links as needed -->
         </ul>
     </nav>
